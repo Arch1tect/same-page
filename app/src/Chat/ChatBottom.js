b13355fca904760c995cd1e657255eb0f0901c85
ArchITech <swtdavid@gmail.com>
2019-04-09 12:05:02 -0700

    display msg right after enter

--- app/src/Chat/ChatBottom.js
+++ app/src/Chat/ChatBottom.js
@@ -2,14 +2,48 @@ import React, { Component } from "react"
 import { Input, Icon } from "antd"
 import "./ChatBottom.css"
 
+const MESSAGE_TIME_GAP = 2 * 1000
+let lastMsgTime = 0
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      input: ""
+    }
+  }
+
+  handleKeyDown = e => {
+    if (e.key === "Enter") {
+      const now = new Date()
+      if (now - lastMsgTime > MESSAGE_TIME_GAP) {
+        let msg = {
+          text: this.state.input,
+          username: "David",
+          self: true
+        }
+        window.addLiveMsgToChatBody(msg)
+        this.setState({ input: "" })
+        lastMsgTime = now
+      } else {
+        console.error("too fast")
+      }
+    }
+  }
+
+  handleChange = e => {
+    this.setState({ input: e.target.value })
+  }
+
   render() {
-    console.log("render chat bottom")
     return (
       <div className="sp-chat-bottom">
         <Input
+          onKeyDown={this.handleKeyDown}
+          value={this.state.input}
           addonBefore={<Icon type="smile" />}
           addonAfter={<Icon type="paper-clip" />}
+          onChange={this.handleChange}
           placeholder="请输入。。。"
         />
       </div>
