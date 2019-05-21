94d53a7bb5e0b63f8b1a02affcc6de112c605ae5
ArchITech <swtdavid@gmail.com>
2019-05-21 02:04:40 -0700

    latest comments

--- app/src/config/urls.js
+++ app/src/config/urls.js
@@ -3,8 +3,8 @@ const urls = {
   // backend should complete url for avatar or any asset
   cloudFront: "https://dnsofx4sf31ab.cloudfront.net/",
   debugMsgSrc: "http://localhost:9009",
-  dbAPI: "https://api-v2.yiyechat.com"
-  // dbAPI: "http://localhost:5000"
+  // dbAPI: "https://api-v2.yiyechat.com"
+  dbAPI: "http://localhost:5000"
 }
 
 export default urls
--- app/src/containers/Home/Comments/Comments.js
+++ app/src/containers/Home/Comments/Comments.js
@@ -1,7 +1,18 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
+
+import { getLatestComments } from "services/comment"
 
 function Comments(props) {
-  return <div>hi</div>
+  const [comments, setComments] = useState([])
+  useEffect(() => {
+    getLatestComments()
+      .then(resp => {
+        setComments(resp.data)
+      })
+      .catch()
+      .then()
+  })
+  return comments.map(comment => <div key={comment.id}>{comment.content}</div>)
 }
 
 export default Comments
