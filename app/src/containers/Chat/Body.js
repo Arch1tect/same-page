7ee27ed0de653b3f092bfbf839fdc86517e40df6
ArchITech <swtdavid@gmail.com>
2019-06-10 02:12:04 -0700

    functional component

--- app/src/containers/Chat/Body.js
+++ app/src/containers/Chat/Body.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect, useContext, useRef } from "react"
 import moment from "moment"
 
 import Message from "./Message"
@@ -18,35 +18,33 @@ const chatBodyStyle = {
 }
 const AUTO_SCROLL_TRESHOLD_DISTANCE = 300
 
-class ChatBody extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { messages: props.data || [] }
-    this.bodyRef = React.createRef()
-    // window.addLiveMsgToChatBody only for debug
-    window.addLiveMsgToChatBody = this.addMsg
-  }
+function ChatBody(props) {
+  const [messages, setMessages] = useState(props.data || [])
+  const bodyRef = useRef(null)
+  const accountContext = useContext(AccountContext)
 
-  componentDidMount() {
-    console.log("register new message handler")
+  useEffect(() => {
     socketHandler.onLiveMsg = data => {
       // message event doesn't contain user data
       // but we should have it in cache when knowing
       // who are in the chatroom
-      data.self = data.userId.toString() === this.context.account.id.toString()
+      data.self =
+        data.userId.toString() === accountContext.account.id.toString()
       const user = getUserFromCache(data.userId)
       data.user = user
       window.parent.postMessage({ ...data, danmu: true }, "*")
       data.time = moment()
-      this.setState((state, props) => ({
-        messages: [...state.messages, data]
-      }))
+      setMessages(prevMessages => {
+        return [...prevMessages, data]
+      })
       let timeout = 10
-      if (data.type === "sticker" || data.type === "image") timeout = 500
-      this.scrollToBottomIfNearBottom(timeout)
+      if (data.type === "sticker") timeout = 500
+      if (data.type === "image") timeout = 700
+      // TODO: use onload event rather than hard code time
+      scrollToBottomIfNearBottom(timeout)
     }
     socketHandler.onRoomChange = roomId => {
-      this.setState({ messages: [] })
+      setMessages([])
     }
     socketHandler.onRecentMessages = recentMessages => {
       // Receive recent messages of the joined room,
@@ -54,19 +52,18 @@ class ChatBody extends React.Component {
       // Shoudn't display recent messages if there's
       // any messages already being displayed, e.g. joined
       // the room then went offline then back online
-      if (this.state.messages.length === 0) {
-        recentMessages.forEach(msg => {
-          msg.self =
-            msg.userId.toString() === this.context.account.id.toString()
-          msg.time = moment.utc(msg.timestamp)
-        })
-        this.setState({ messages: recentMessages })
-      }
+      recentMessages.forEach(msg => {
+        msg.self =
+          msg.userId.toString() === accountContext.account.id.toString()
+        msg.time = moment.utc(msg.timestamp)
+      })
+      setMessages(recentMessages)
     }
-  }
-  scrollToBottomIfNearBottom = timeout => {
+  }, [])
+
+  const scrollToBottomIfNearBottom = timeout => {
     timeout = timeout || 100
-    const bodyDiv = this.bodyRef.current
+    const bodyDiv = bodyRef.current
     if (
       bodyDiv.scrollHeight - bodyDiv.scrollTop - bodyDiv.offsetHeight <
       AUTO_SCROLL_TRESHOLD_DISTANCE
@@ -76,60 +73,51 @@ class ChatBody extends React.Component {
       }, timeout)
     }
   }
-  addMsg = msg => {
-    this.setState((state, props) => ({
-      messages: [...state.messages, msg]
-    }))
-  }
 
-  render() {
-    let messages = []
-    let lastMsg = null
-    let index = 0
+  let res = []
+  let lastMsg = null
+  let index = 0
 
-    this.state.messages.forEach(msg => {
-      // If same user is talking, no need to show user's avatar again
-      let showUser = true
-      // If it's been more than 5 mins since last msg
-      let showTimestamp = false
-      let timeDisplay = null
+  messages.forEach(msg => {
+    // If same user is talking, no need to show user's avatar again
+    let showUser = true
+    // If it's been more than 5 mins since last msg
+    let showTimestamp = false
+    let timeDisplay = null
 
-      if (lastMsg) {
-        if (lastMsg.userId.toString() === msg.userId.toString())
-          showUser = false
-        if (msg.time.diff(lastMsg.time) > 5 * 60 * 1000) {
-          showTimestamp = true
-          showUser = true
-        }
-      } else {
+    if (lastMsg) {
+      if (lastMsg.userId.toString() === msg.userId.toString()) showUser = false
+      if (msg.time.diff(lastMsg.time) > 5 * 60 * 1000) {
         showTimestamp = true
         showUser = true
       }
+    } else {
+      showTimestamp = true
+      showUser = true
+    }
 
-      if (showTimestamp) {
-        if (moment().diff(msg.time) > 24 * 60 * 60 * 1000)
-          timeDisplay = msg.time.local().format("MMMDo A HH:mm")
-        else timeDisplay = msg.time.local().format("A HH:mm")
-      }
+    if (showTimestamp) {
+      if (moment().diff(msg.time) > 24 * 60 * 60 * 1000)
+        timeDisplay = msg.time.local().format("MMMDo A HH:mm")
+      else timeDisplay = msg.time.local().format("A HH:mm")
+    }
 
-      messages.push(
-        <Message
-          withHoverCard={true}
-          key={index++}
-          data={msg}
-          showUser={showUser}
-          timeDisplay={timeDisplay}
-        />
-      )
-      lastMsg = msg
-    })
-    return (
-      <div ref={this.bodyRef} style={chatBodyStyle}>
-        {messages}
-      </div>
+    res.push(
+      <Message
+        withHoverCard={true}
+        key={index++}
+        data={msg}
+        showUser={showUser}
+        timeDisplay={timeDisplay}
+      />
     )
-  }
+    lastMsg = msg
+  })
+  return (
+    <div ref={bodyRef} style={chatBodyStyle}>
+      {res}
+    </div>
+  )
 }
-ChatBody.contextType = AccountContext
 
 export default ChatBody
