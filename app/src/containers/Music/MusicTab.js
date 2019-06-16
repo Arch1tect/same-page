9ae3101cd34e72ee7746bd845301f0de533a8fa0
ArchITech <swtdavid@gmail.com>
2019-06-15 20:22:03 -0700

    show help modal for player

--- app/src/containers/Music/MusicTab.js
+++ app/src/containers/Music/MusicTab.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react"
-import { Button, Radio } from "antd"
+import { Button, Radio, Modal } from "antd"
 
 import MusicPlayer from "components/MusicPlayer"
 import Playlist from "./Playlist"
 
 function MusicTab(props) {
   const [loopMode, setLoopMode] = useState("loopAll")
+  const [showHelp, setShowHelp] = useState(false)
   return (
     <div className="sp-special-tab">
       <Button
@@ -34,6 +35,55 @@ function MusicTab(props) {
           <Radio.Button value="loopCurrent">循环当前</Radio.Button>
           <Radio.Button value="loopAll">循环列表</Radio.Button>
         </Radio.Group>
+        <Button
+          style={{ border: "none", position: "absolute", right: 0 }}
+          onClick={() => {
+            setShowHelp(true)
+          }}
+          size="small"
+          icon="question"
+        />
+        <Modal
+          title="播放器使用指南"
+          visible={showHelp}
+          onCancel={() => {
+            setShowHelp(false)
+          }}
+          wrapClassName="sp-modal"
+          footer={null}
+          bodyStyle={{ maxHeight: "calc(100% - 55px)", overflowY: "auto" }}
+        >
+          <h3>分享原始资源</h3>
+          <p>输入原始的视频或音频文件地址，比如</p>
+          <p style={{ fontSize: 10 }}>
+            https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_5mb.mp4
+          </p>
+          <h4>支持以下视频格式：</h4>
+          <ul>
+            <li>mp4</li>
+            <li>webm</li>
+            <li>ogg</li>
+            <li>flv</li>
+          </ul>
+          <h4>支持以下音频格式：</h4>
+          <ul>
+            <li>mp3</li>
+            <li>wav</li>
+          </ul>
+
+          <br />
+          <h3>分享网站链接</h3>
+          <p>直接输入含有视频或音频的网页地址，比如</p>
+          <p style={{ fontSize: 10 }}>https://music.163.com/#/song?id=640565</p>
+          <p style={{ fontSize: 10 }}>
+            https://www.youtube.com/watch?v=9bZkp7q19f0
+          </p>
+          <h4>支持的网站有：</h4>
+          <ul>
+            <li>Youtube</li>
+            <li>网易云音乐</li>
+          </ul>
+        </Modal>
       </center>
       <div className="sp-tab-body" style={{ background: "#404040" }}>
         <MusicPlayer />
